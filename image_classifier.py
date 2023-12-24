3fa86ce0e33d3031596121d5d148587288d760aa
eramon225 <154682372+eramon225@users.noreply.github.com>
2023-12-24 16:19:04 -0600

    Update image_classifier.py

--- image_classifier.py
+++ image_classifier.py
@@ -9,21 +9,26 @@ class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
 model = load_model('flower_model.h5')
  
 # Predict using the model
-#sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-#sunflower_url = "https://s3.amazonaws.com/cdn.tulips.com/images/popup/Jumbo-Pink-Triumph-Tulip-close-up.jpg"
-sunflower_url = "https://imageio.forbes.com/specials-images/imageserve/6064b148afc9b47d022718d1/Hennessey-Venom-F5/960x0.jpg?height=473&width=711&fit=bounds"
-sunflower_path = tf.keras.utils.get_file(origin=sunflower_url)
+sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
 
-img = tf.keras.utils.load_img(
-    sunflower_path, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
+def classifyImage(url, options):
+    sunflower_path = tf.keras.utils.get_file(origin=url)
 
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+    img = tf.keras.utils.load_img(
+        sunflower_path, target_size=(img_height, img_width)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+
+    return {'obj': options[np.argmax(score)], 'confidence': 100 * np.max(score)}
+
+if __name__ == "__main__":
+    result = classifyImage( sunflower_url, class_names )
+
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(result['obj'], result['confidence'])
+    )
