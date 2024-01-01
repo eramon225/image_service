2159be7589a02a7824c6ba7bb4a9a051ac1567a9
eramon225 <eramon224@live.com>
2024-01-01 10:29:39 -0600

    Changed to accept image bytes

--- detector.py
+++ detector.py
@@ -6,15 +6,14 @@ api_key = 'acc_c4be0283972f62a'
 api_secret = '7327da1e5572b47ba66dd69c28790a97'
 
 ## TODO: Consider using try/except here
-def detect(image_path):
-    if validators.url(image_path):
+def detect(image_file):
+    if validators.url(image_file):
         response = requests.get(
-            'https://api.imagga.com/v2/tags?image_url=%s' % image_path,
+            'https://api.imagga.com/v2/tags?image_url=%s' % image_file,
             auth=(api_key, api_secret)
         )
         return response.json()
     else:
-        image_file = open(image_path, 'rb')
         response = requests.post(
             'https://api.imagga.com/v2/tags',
             auth=(api_key, api_secret),
