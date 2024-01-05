41c7e604325f6b4c8ea61776a0e97623b4568d14
eramon225 <eramon224@live.com>
2024-01-04 20:09:41 -0600

    Enhanced to accept args for more flexible posts

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
+import argparse
 
-def run_image(payload):
+def post_image(payload):
     try:
         url = 'http://localhost:5000/images'
 
@@ -9,22 +10,48 @@ def run_image(payload):
         else:
             response = requests.post(url, json=payload)
 
-        print(response.status_code)
-        # print(response.json())
+        return response
     except Exception as ex:
         print(f"Uncaught Exception: {ex}")
 
+def main():
+    parser = argparse.ArgumentParser(description='A simple command-line argument parser for sending images.')
+    
+    # Adding arguments
+    parser.add_argument('-p', '--path', type=str, help='url or local location of valid image')
+    parser.add_argument('-l', '--label', type=str, help='label for image')
+    parser.add_argument('-f', '--file', type=str, help='local location of valid image posted via file')
+    parser.add_argument('-d', '--detect', action='store_true', help='Flag for triggering detection')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Flag for verbose printing')
+
+    # Parsing arguments
+    args = parser.parse_args()
+
+    payload = {
+        "detect": args.detect
+    }
+
+    if args.path or args.label or args.file:
+        # Accessing arguments
+        if args.path:
+            payload['path'] = args.path
+        if args.label:
+            payload['label'] = args.label
+        
+        # If the file option is filled out, just send the entire file
+        # and overwrite other options
+        if args.file:
+            payload = {
+                "file": open(args.file, 'rb')
+            }
+        if args.verbose:
+            print(payload)
+        response = post_image(payload)
+        print(response.status_code)
+        if args.verbose:
+            print(response.json())
+    else:
+        print("Add path, or file of image to use this script! Use --help for details")
+
 if __name__ == "__main__":
-    run_image({
-        "file": open( "C:/Users/Eric/Downloads/dog.jpg", "rb" )
-    })
-    run_image({
-        "path": "C:/Users/Eric/Downloads/dog.jpg",
-        "detect": False
-    })
-    run_image({
-        "path": "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2022/6/16/1/shutterstock_1862856634.jpg.rend.hgtvcom.1280.853.suffix/1655430860853.jpeg",
-        "detect": False
-    })
-
-
+    main()
